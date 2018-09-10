dff2dea20bfa7e4702660c4379a017c32dbc9112
Joseantpr <joseantpalaciosramirez@gmail.com>
2018-09-10 16:13:49 +0200

    ADD: Header & Form

--- cotizador/src/components/App.js
+++ cotizador/src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import Formulario from './Formulario';
 
 class App extends Component {
   render() {
     return (
-     <div className="contenedor">
-      <h1>Desde el componente</h1>
+     <div className='contenedor'>
+      <Header titulo='Cotizador de Seguro de Coche'/>
+      <div className='contenedor-formulario'>
+        <Formulario />
+      </div>
      </div>
     );
   }
